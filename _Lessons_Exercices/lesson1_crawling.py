f387133f434b725ff52b5ef7753708191866c09d
Charles Miglietti <charles.miglietti@yahoo.fr>
2018-09-28 16:23:00 +0200

    add exemple for share count

--- _Lessons_Exercices/lesson1_crawling.py
+++ _Lessons_Exercices/lesson1_crawling.py
@@ -16,5 +16,14 @@ if res.status_code == 200:
   print all_links
 
 
+url_page = "http://www.purepeople.com/article/brigitte-macron-decroche-une-jolie-couv-a-l-etranger_a306389/1"
+
+res = requests.get(url_page)
+
+if res.status_code == 200:
+  html_doc =  res.text
+  soup = BeautifulSoup(html_doc,"html.parser")
+  specific_class = "c-sharebox__stats-number"
+  print soup.find("span", class_= specific_class).text
 
 
