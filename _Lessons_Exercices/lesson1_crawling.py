a4d27670407c411681a74f9638ad1b05eb2d3417
Charles Miglietti <charles.miglietti@yahoo.fr>
2018-09-28 16:24:06 +0200

    iterate for complete exemple

--- _Lessons_Exercices/lesson1_crawling.py
+++ _Lessons_Exercices/lesson1_crawling.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 import requests
+import unittest
 from bs4 import BeautifulSoup
 
-
+website_prefix = "http://www.purepeople.com"
 def _handle_request_result_and_build_soup(request_result):
 
   if request_result.status_code == 200:
@@ -12,7 +13,7 @@ def _handle_request_result_and_build_soup(request_result):
 
 def get_all_links_for_query(query):
 
-  url = "http://www.purepeople.com/rechercher/"
+  url = website_prefix + "/rechercher/"
   res = requests.post(url, data = {'q': query })
   soup = _handle_request_result_and_build_soup(res)
   specific_class = "c-article-flux__title"
@@ -24,20 +25,25 @@ def get_share_count_for_page(page_url):
   res = requests.get(page_url)
   soup = _handle_request_result_and_build_soup(res)
   specific_class = "c-sharebox__stats-number"
-  return soup.find("span", class_= specific_class).text
+  share_count_text = soup.find("span", class_= specific_class).text
+  return  int(share_count_text.strip())
 
 
 query = 'macron'
 page_url = "http://www.purepeople.com/article/brigitte-macron-decroche-une-jolie-couv-a-l-etranger_a306389/1"
 
 url_macron = get_all_links_for_query(query)
-share_count_test = get_share_count_for_page(page_url)
+results_macron = []
+for url in url_macron:
+    results_macron.append(get_share_count_for_page(website_prefix + url))
 
 class Lesson1Tests(unittest.TestCase):
     def testShareCount(self):
         self.assertEqual(get_share_count_for_page("http://www.purepeople.com/article/brigitte-macron-decroche-une-jolie-couv-a-l-etranger_a306389/1") , 86)
 
 
-
 def main():
     unittest.main()
+
+#if __name__ == '__main__':
+    #main()
