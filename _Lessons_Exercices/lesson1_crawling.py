79ce8bfba18f4b871de53fc8de1cd459d15edd7b
Charles Miglietti <charles.miglietti@yahoo.fr>
2018-09-28 16:23:18 +0200

    refacto

--- _Lessons_Exercices/lesson1_crawling.py
+++ _Lessons_Exercices/lesson1_crawling.py
@@ -3,27 +3,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "http://www.purepeople.com/rechercher/"
 
-#res = requests.get(url)
-res = requests.post(url, data = {'q':'macron'})
+def get_all_links_for_query(query):
 
-if res.status_code == 200:
-  html_doc =  res.text
-  soup = BeautifulSoup(html_doc,"html.parser")
-  specific_class = "c-article-flux__title"
-  all_links = map(lambda x : x.attrs['href'] , soup.find_all("a", class_= specific_class))
-  print all_links
+  url = "http://www.purepeople.com/rechercher/"
+  res = requests.post(url, data = {'q': query })
 
+  if res.status_code == 200:
+    html_doc =  res.text
+    soup = BeautifulSoup(html_doc,"html.parser")
+    specific_class = "c-article-flux__title"
+    all_links = map(lambda x : x.attrs['href'] , soup.find_all("a", class_= specific_class))
 
-url_page = "http://www.purepeople.com/article/brigitte-macron-decroche-une-jolie-couv-a-l-etranger_a306389/1"
+  return all_links
 
-res = requests.get(url_page)
+page_url = "http://www.purepeople.com/article/brigitte-macron-decroche-une-jolie-couv-a-l-etranger_a306389/1"
+def get_share_count_for_page(page_url):
 
-if res.status_code == 200:
-  html_doc =  res.text
-  soup = BeautifulSoup(html_doc,"html.parser")
-  specific_class = "c-sharebox__stats-number"
-  print soup.find("span", class_= specific_class).text
+  res = requests.get(page_url)
+
+  if res.status_code == 200:
+    html_doc =  res.text
+    soup = BeautifulSoup(html_doc,"html.parser")
+    specific_class = "c-sharebox__stats-number"
+    return soup.find("span", class_= specific_class).text
 
 
